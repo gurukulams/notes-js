f9470afc6b130d05b015d3d62dfe153fc7ab2468
sathishk <sathishkumar.thiyagarajan@gmail.com>
2026-05-26 15:51:05 +0530

    Small CHanges

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,17 +1,25 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-
 import replace from '@rollup/plugin-replace';
 import postcss from 'rollup-plugin-postcss';
 
 export default {
   input: 'src/notes.js',
+  
+  // 1. Tell Rollup NOT to include Bootstrap in the bundle
+  external: ['bootstrap', '@popperjs/core'], 
+
   output: [
     {
       file: 'dist/notes.bundle.js',
       format: 'umd',
       name: 'NotesMaker',
-      sourcemap: true
+      sourcemap: true,
+      // 2. Map the external imports to global variables for the UMD build
+      globals: {
+        'bootstrap': 'bootstrap',
+        '@popperjs/core': 'Popper'
+      }
     },
     {
       file: 'dist/notes.esm.js',
@@ -20,16 +28,14 @@ export default {
     }
   ],
   plugins: [
-    // 2. Define process.env.NODE_ENV
     replace({
       preventAssignment: true,
       'process.env.NODE_ENV': JSON.stringify('production')
     }),
-     postcss({
-      extract: true,   // 👈 IMPORTANT
+    postcss({
+      extract: true,
       minimize: true
     }),
-
     resolve({
       browser: true,
       preferBuiltins: false
